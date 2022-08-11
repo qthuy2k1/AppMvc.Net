32fa1407de0ffc35b6f3d216129b88155493a4e4
qtplus123 <qthuy46@gmail.com>
2022-08-12 00:03:32 +0700

    View Product, Cart

---
+++ Areas/Product/Models/CartItem.cs
@@ -0,0 +1,11 @@
+
+using App.Models.Product;
+
+namespace AppMvc.Areas.Product.Models
+{
+    public class CartItem
+    {
+        public int quantity { set; get; }
+        public ProductModel product { set; get; }
+    }
+}
