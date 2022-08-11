fff5c4280688eff175d582a7680d8ce049a48817
qtplus123 <qthuy46@gmail.com>
2022-08-11 09:59:13 +0700

    Product Manage

---
+++ Models/Product/ProductPhoto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App.Models.Product
+{
+    [Table("ProductPhoto")]
+    public class ProductPhoto
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FileName { get; set; }
+
+        public int ProductID { get; set; }
+        [ForeignKey("ProductID")]
+        public ProductModel Product { get; set; }
+    }
+}
